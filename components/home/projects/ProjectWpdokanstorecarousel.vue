430b838946e912e08d22226d1544f8ef6bbbc7b4
Niz <62140571+nicosapp@users.noreply.github.com>
2020-11-17 12:23:34 +0100

    fix case error

---
+++ components/home/projects/ProjectWpdokanstorecarousel.vue
@@ -0,0 +1,36 @@
+<template>
+  <ProjectTemplate
+    title="Dokan Store Carousel"
+    link-github-client="https://github.com/nicosapp/niz_dokansc"
+    link-demo="https://fr.wordpress.org/plugins/niz-stores-carousel-for-dokan/"
+    :bg-class="bgClass"
+    :text-first="true"
+  >
+    {{ $t('projects.dokanstorecarousel.description') }}
+
+    <template v-slot:images>
+      <v-row class="fill-height">
+        <v-col cols="12">
+          <ProjectImage src="dokansc-1.JPG" :max-height="300" />
+        </v-col>
+      </v-row>
+    </template>
+  </ProjectTemplate>
+</template>
+
+<script>
+export default {
+  props: {
+    bgClass: {
+      required: false,
+      type: String,
+      default: 'bg',
+    },
+  },
+  data() {
+    return {
+      chips: ['Wordpress', 'Dokan', 'jQuery', 'Owl'],
+    }
+  },
+}
+</script>
